0fb69c0af64a9c573e545b64076eddbe09d46aa3
Dragan Pilipovic <draganpilipovic1997bp@gmail.com>
2023-07-15 17:50:46 +0200

    fq implementation using lambdaworks lib

---
+++ src/fields/fq.rs
@@ -0,0 +1,4 @@
+
+//! Base field of bandersantch -- which is also the scalar field of Bls12-381 curve.
+
+pub use lambdaworks_math::elliptic_curve::short_weierstrass::curves::bls12_381::default_types::FrField;
