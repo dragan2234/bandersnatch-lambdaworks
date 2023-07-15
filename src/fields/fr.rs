718ba6f572a4509d5b4bc7f1a974f8f6821278ee
Dragan Pilipovic <draganpilipovic1997bp@gmail.com>
2023-07-15 18:41:54 +0200

    fix comments

--- src/fields/fr.rs
+++ src/fields/fr.rs
@@ -12,14 +12,14 @@ use lambdaworks_math::{
 #[derive(Clone, Debug)]
 pub struct FrConfig;
 
-/// Modulus of bls 12 381 subgroup
+/// Modulus of bandersnatch subgroup
 impl IsModulus<U256> for FrConfig {
     const MODULUS: U256 = U256::from_hex_unchecked(
         "1CFB69D4CA675F520CCE760202687600FF8F87007419047174FD06B52876E7E1",
     );
 }
 
-/// FrField using MontgomeryBackend for bls 12 381
+/// FrField using MontgomeryBackend for bandersnatch
 pub type FrField = MontgomeryBackendPrimeField<FrConfig, 4>;
-/// FrElement using MontgomeryBackend for bls 12 381
+/// FrElement using MontgomeryBackend for bandersnatch
 pub type FrElement = FieldElement<FrField>;
